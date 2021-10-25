aee50acaea7d2a565eb03f08722b69946e45a3f7
Baby Karuppusamy <91045121+Baby-Karuppusamy@users.noreply.github.com>
2021-10-25 13:20:09 +0530

    Create LEDAlert.java

---
+++ src/main/java/statisticker/LEDAlert.java
@@ -0,0 +1,8 @@
+class LEDAlert implements IAlerter{
+    boolean ledGlows = false;
+    @Override
+    public void setAlert(boolean bool) {
+        // TODO Auto-generated method stub
+        this.ledGlows = bool;
+    }
+}
