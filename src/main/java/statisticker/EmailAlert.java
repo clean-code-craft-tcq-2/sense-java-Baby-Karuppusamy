cb762796a503f011fb88ce880eb57dfcefbb0463
Baby Karuppusamy <91045121+Baby-Karuppusamy@users.noreply.github.com>
2021-10-25 13:19:16 +0530

    Create EmailAlert.java

---
+++ src/main/java/statisticker/EmailAlert.java
@@ -0,0 +1,8 @@
+class EmailAlert implements IAlerter{
+    boolean emailSent = false;
+    @Override
+    public void setAlert(boolean bool) {
+        // TODO Auto-generated method stub
+        this.emailSent = bool;
+    }
+}
